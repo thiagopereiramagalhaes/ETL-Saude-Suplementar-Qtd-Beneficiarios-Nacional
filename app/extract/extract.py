0b553135d0a84776f285a1aa19fb33d26a75049e
Thiago Pereira Magalhães <magalhaes.thiagopm@outlook.com>
2024-12-09 17:54:12 -0300

    Update extract.py

--- app/extract/extract.py
+++ app/extract/extract.py
@@ -4,18 +4,18 @@ import pandas as pd
 import time
 from tqdm import tqdm 
 from io import BytesIO
-from config.config import Config
-from models.periodo_processado import Periodo_Processado as pp
-from config.monitor import Monitor_Memory
-from transform.transform import Transform
+from config import config
+from models import periodo_processado
+from config import monitor
+from transform import transform
 
 class Extract():
     def __init__(self):
-        self.list_tasks = Config().list_tasks
-        self.base_url = Config().base_url
-        self.insert_last_date = pp().insert_last_date()
-        self.monitor_memory = Monitor_Memory().monitor_memory()
-        self.transform = Transform().transform()
+        self.list_tasks = config.Config().list_tasks
+        self.base_url = config.Config().base_url
+        self.insert_last_date = periodo_processado.Periodo_Processado()
+        self.monitor_memory = monitor.Monitor_Memory()
+        self.transform = transform.Transform()
 
     def extract(self):
         with tqdm(total=len(self.list_tasks), desc="Baixando dados", unit="dados") as pbar:
@@ -29,13 +29,13 @@ class Extract():
                                     if file.endswith('.csv'):
 
                                         with z.open(file) as csv_file:
-                                            self.transform(csv_file, year, month, state)
+                                            self.transform.transform(csv_file, year, month, state)
                                             
 
                     except Exception as e:
                         print(f"Erro ao processar {url}: {e}")
-                        self.insert_last_date(year, month, state, e)
+                        self.insert_last_date.insert_last_date(year, month, state, e)
                     
                     pbar.update(1)
                     time.sleep(3)
-                    self.monitor_memory()
+                    self.monitor_memory.monitor_memory()
